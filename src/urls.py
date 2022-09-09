2331399ad0d52087267cca3f8ec9e7b360c8383d
X3NNY <xenny@vip.qq.com>
2022-09-10 01:53:20 +0800

    fix a error.

--- src/urls.py
+++ src/urls.py
@@ -2,27 +2,27 @@ from handler import *
 from settings import host_exp, api_host_exp, self_exp
 
 urls = [
-    (r'/register/', RegisterHandler, api_host_exp),
-    (r'/login/', LoginHandler, api_host_exp),
-    (r'/reapply/', ReapplyHandler, api_host_exp),
-    (r'/change/', ChangeHandler, api_host_exp),
+    (r'/register/', RegisterHandler, self_exp),
+    (r'/login/', LoginHandler, self_exp),
+    (r'/reapply/', ReapplyHandler, self_exp),
+    (r'/change/', ChangeHandler, self_exp),
 
-    (r'/info/', InfoHandler, api_host_exp),
-    (r'/info/routes/', InfoRoutesHandler, api_host_exp),
-    (r'/info/(?P<hid>\d+)/detail/', HttpLogDetailHandler, api_host_exp),
-    (r'/list/(?P<page>\d+)/(?P<size>\d+)/', RequestListHandler, api_host_exp),
+    (r'/info/', InfoHandler, self_exp),
+    (r'/info/routes/', InfoRoutesHandler, self_exp),
+    (r'/info/(?P<hid>\d+)/detail/', HttpLogDetailHandler, self_exp),
+    (r'/list/(?P<page>\d+)/(?P<size>\d+)/', RequestListHandler, self_exp),
 
-    (r'/search/ct/', SearchCTHandler, api_host_exp),
-    (r'/search/ip/', SearchIPHandler, api_host_exp),
+    (r'/search/ct/', SearchCTHandler, self_exp),
+    (r'/search/ip/', SearchIPHandler, self_exp),
 
-    (r'/download/', DownloadHandler, api_host_exp),
-    (r'/download/all/', DownloadAllHandler, api_host_exp),
-    (r'/delete/', DeleteHandler, api_host_exp),
-    (r'/delete/all/', DeleteAllHandler, api_host_exp),
+    (r'/download/', DownloadHandler, self_exp),
+    (r'/download/all/', DownloadAllHandler, self_exp),
+    (r'/delete/', DeleteHandler, self_exp),
+    (r'/delete/all/', DeleteAllHandler, self_exp),
 
-    (r'/route/(?P<rid>\d+)/', RouteHandler, api_host_exp),
-    (r'/route/add/', RouteAddHandler, api_host_exp),
-    (r'/file/(?P<hash>[0-9a-f]+)/download/', FileDownloadHandler, api_host_exp),
+    (r'/route/(?P<rid>\d+)/', RouteHandler, self_exp),
+    (r'/route/add/', RouteAddHandler, self_exp),
+    (r'/file/(?P<hash>[0-9a-f]+)/download/', FileDownloadHandler, self_exp),
 
     (r'/', HTMLHandler, self_exp),
     
